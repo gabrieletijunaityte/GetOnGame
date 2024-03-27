79ca192848f07b03be4abc0dc36ef8b46e2523d2
Nik Verweel <nik.verweel@wur.nl>
2024-03-27 16:32:39 +0100

    NV JW Created Rules class
    Created the rules class, by default initialized based on a text file.
    Method to view the files included for user story #8.

---
+++ GetOn/src/rules/Rules.java
@@ -0,0 +1,43 @@
+package rules;
+
+// Import required functionalities
+import java.io.IOException;
+import java.lang.IllegalStateException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+
+// Create rule class
+public class Rules {
+	
+	// Set the default rules of the game
+	private String rules = readRules();
+	
+	// Method to load the rules from the data folder
+	private static String readRules() {
+		
+		String defaultRules = "";
+		
+		try (Scanner input = new Scanner(Paths.get("data/rules.txt"))) {
+			
+			while (input.hasNext()) {
+				defaultRules += " ";
+				defaultRules += input.next();
+			}
+			
+		} catch (NoSuchElementException | IllegalStateException | IOException e) {
+			System.err.println("Error reading text file. Terminating.");
+		}
+		
+		return defaultRules;
+		
+	}
+	
+	// Method to show the rules to a spectator
+	public String getRules() {
+		return rules;
+	}
+	
+}
---
+++ GetOn/src/rules/RulesTest.java
@@ -0,0 +1,25 @@
+package rules;
+
+import junit.framework.TestCase;
+
+public class RulesTest extends TestCase {
+
+	// Testing the creation of the default rule object
+	public void testRuleCreation() {
+		
+		Rules defaultRule = new Rules();
+		assertNotNull(defaultRule);
+		
+	}
+	
+	// Testing the retrieving of the default rule object
+	public void testGetRules() {
+		
+		Rules defaultRule = new Rules();
+		String returned = defaultRule.getRules();
+		assertNotNull(returned);
+		System.out.println(returned);
+		
+	}
+	
+}
