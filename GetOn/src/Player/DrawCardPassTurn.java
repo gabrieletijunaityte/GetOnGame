a2755e28e609e9c13a0305af9ab3978e4820ec93
sriramjallu <sriram.jallu@wur.nl>
2024-03-27 19:50:14 +0100

    GT SJ Added the changeTurn method to change the Turn status of the player + Added a TDD method testPassTurn to Pass the turn of the player

--- GetOn/src/Player/DrawCardPassTurn.java
+++ GetOn/src/Player/DrawCardPassTurn.java
@@ -7,22 +7,43 @@ import junit.framework.TestCase;
 
 public class DrawCardPassTurn extends TestCase {
 	
+	private ArrayList<Player> players = new ArrayList<>();
+	private ArrayList<Card> stack = new ArrayList<>();
+	
+	
 	public void testPlayerCreation() {
 		
-		Player player1 = new Player();
-		assertNotNull(player1);
+		ArrayList<Player> players = new ArrayList<>();
+		assertNotNull(players);
 		
 	}
 	
 	
-	
 	public void testDrawCard() {
 		
-		Player player1 = new Player();
-		ArrayList<Card> stack = new ArrayList<>();
+
+		Player player1 = new Player("Player1", true);
+		
 		stack.add(new Card("test1"));
 		stack.add(new Card("test2"));
 		player1.drawCard(stack);
+		
+	}
+	
+	
+	public void testPassTrun() {
+		
+
+		players.add(new Player("abc", true));
+		players.add(new Player("dfa", false));
+		
+		stack.add(new Card("test1"));
+		stack.add(new Card("test2"));
+		
+		players.get(0).drawCard(stack);
+		players.get(0).changeTurn();
+		players.get(1).changeTurn();
+		
 		
 	}
 
--- GetOn/src/Player/Player.java
+++ GetOn/src/Player/Player.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Player {
 	
 	private ArrayList<Card> hand = new ArrayList<>();
+	private Boolean isTurn;
+	private String name;
+	
+	public Player(String name, Boolean isTurn) {
+		this.name = name;
+		
+		this.isTurn = true;
+	}
 	
 
 	public void drawCard(ArrayList<Card> stack) {
@@ -20,6 +28,23 @@ public class Player {
 		
 		System.out.print(stack.get(stack.size()-1).getCardName());
 	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void changeTurn() {
+		
+		if (isTurn) {
+			this.isTurn = false;
+		}
+		else {
+			this.isTurn = true;
+		}
+	}
+	
+
+
 	
 	// Dummy Class for Player
 
