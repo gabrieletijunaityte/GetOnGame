9408d0f3f8e1879f4a8a537bf372887cb2707bf4
Mirte <mirte.riemens@wur.nl>
2024-03-28 15:21:40 +0100

    MR local pull to PersonClass

---
+++ GetOn/src/logic/Player.java
@@ -0,0 +1,51 @@
+package logic;
+import java.util.ArrayList;
+
+
+public class Player {
+	
+	private ArrayList<Card> hand = new ArrayList<>();
+	private Boolean isTurn;
+	private String name;
+	
+	public Player(String name, Boolean isTurn) {
+		this.name = name;
+		
+		this.isTurn = true;
+	}
+	
+
+	public void drawCard(ArrayList<Card> stack) {
+		// TODO Auto-generated method stub
+		
+		System.out.print(stack.get(stack.size()-1).getCardName());
+		
+//		System.out.print(stack[stack.length-1].getCardName());
+		hand.add(stack.get(stack.size()-1));
+		System.out.print(hand.get(hand.size()-1).getCardName());
+		
+		stack.remove(stack.get(stack.size()-1));
+		
+		System.out.print(stack.get(stack.size()-1).getCardName());
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void changeTurn() {
+		
+		if (isTurn) {
+			this.isTurn = false;
+		}
+		else {
+			this.isTurn = true;
+		}
+	}
+	
+
+
+	
+	// Dummy Class for Player
+
+}
