1f5cb18ab9f4b6f347f3ea83530485990417c099
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-02 19:41:41 +0200

    YM GT Game class created by TDD approch together with gameTest test. This test tests if class was created

---
+++ GetOn/src/test/gameTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import game.Game;
+import junit.framework.TestCase;
+
+public class gameTest extends TestCase {
+	
+	public void testGameCreation () {
+		// Creating game class
+		Game game1 = new Game();
+		
+		// asserting if it was created
+		assertNotNull(game1);
+	}
+	
+	
+
+}
