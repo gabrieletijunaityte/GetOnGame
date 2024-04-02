7da303ecc7c307f6afb865b51af1cfffe3218daa
bjwwaterreus <bart.waterreus@wur.nl>
2024-04-02 16:17:59 +0200

    BW JW Added comments to Spectator and SpectatorTest classes

--- GetOn/src/logic/Spectator.java
+++ GetOn/src/logic/Spectator.java
@@ -1,7 +1,11 @@
 package logic;
 
+
+// Create spectator class
 public class Spectator {
 
+	/* Method to create a rules object, and use getRules() from the Rules Class
+	to get the rules and return them*/
 	public String viewRules() {
 		Rules rules = new Rules();
 		String rulesOut = rules.getRules();
--- GetOn/src/test/SpectatorTest.java
+++ GetOn/src/test/SpectatorTest.java
@@ -3,6 +3,11 @@ package test;
 import junit.framework.TestCase;
 import logic.Spectator;
 
+
+/* Create test spectator object
+ * use its viewRules() method and store the rules into a test return object
+ * print out the return object to visualize thge rules
+ */
 public class SpectatorTest extends TestCase {
 	
 	public void testViewRules() {
