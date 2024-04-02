04c2236e459d8cd0c8b5cea9a8deb183b668e4b4
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-02 20:53:48 +0200

    YM GT in game player is initiated by entering players name. He gets 5 cards, and his hand is shown in console

--- GetOn/src/game/Game.java
+++ GetOn/src/game/Game.java
@@ -1,8 +1,38 @@
 package game;
+import logic.Card;
+import logic.Player;
+import logic.Type;
+import logic.Value;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Enter player name: ");
+		String playerName = input.next(); 
+		
+		Player firstPlayer = new Player(playerName, true);
+		
+		// Dummy stack
+		
+		ArrayList<Card> stack = new ArrayList<>();
+		
+		// Create 5 cards to test the set up
+		stack.add(new Card(Type.getType(0), Value.getValue(0)));
+		stack.add(new Card(Type.getType(1), Value.getValue(1)));
+		stack.add(new Card(Type.getType(2), Value.getValue(0)));
+		stack.add(new Card(Type.getType(0), Value.getValue(1)));
+		stack.add(new Card(Type.getType(0), Value.getValue(1)));
+		stack.add(new Card(Type.getType(1), Value.getValue(0)));
+		
+		// See players hand:
+		firstPlayer.drawCard(stack, 5);
+		System.out.print("The player's hand contains: ");
+		System.out.print(firstPlayer.viewHand());
 		
 
 	}
