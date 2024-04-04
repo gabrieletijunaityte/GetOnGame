b73de58aa1cbb20e412face7c7f4362f91fef91a
Lennart Murk <lennart.murk@wur.nl>
2024-04-04 17:11:56 +0200

    NT LM added an image to the GUI and to the data folder. The image is not completely visible in the GUI.

--- GetOn/src/graphics/GameFrame.java
+++ GetOn/src/graphics/GameFrame.java
@@ -5,7 +5,9 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,17 +15,29 @@ import javax.swing.JLabel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import javax.swing.SwingConstants;
 import java.awt.GridLayout;
+import java.awt.Image;
+
+import logic.KilometerCard;
+
 
 public class GameFrame extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
     private JPanel kilometerPanel;
-    private JPanel topCardPanel;
+    protected String topCard = "Discarded cards"; //test Variable to test the discard pile
+    protected int numberof5cards = 1;
+    protected int numberof6cards = 2;
+    protected int numberof8cards = 3;
+    protected int numberof10cards = 4;
 
-
+    private KilometerCard kilometerCardInstance = new KilometerCard(5);
     
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -46,9 +60,9 @@ public class GameFrame extends JFrame {
         setContentPane(contentPane);
         GridBagLayout gbl_contentPane = new GridBagLayout();
         gbl_contentPane.columnWidths = new int[]{213, 213, 0}; 
-        gbl_contentPane.rowHeights = new int[]{14, 20, 0, 224, 0, 0};
+        gbl_contentPane.rowHeights = new int[]{14, 20, 0, 224, 0, 0, 0};
         gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE}; 
-        gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
         contentPane.setLayout(gbl_contentPane);
         
         JLabel lblNewLabel = new JLabel("GameTable");
@@ -69,8 +83,11 @@ public class GameFrame extends JFrame {
         contentPane.add(discardPanel, gbc_discardPanel);
         discardPanel.setLayout(new GridLayout(1, 0, 0, 0));
         
-        JLabel discardPile = new JLabel("Discard pile");
+        JLabel discardPile = new JLabel("Discard pile: ");
         discardPanel.add(discardPile);
+        
+        JLabel discardRepresentation = new JLabel(topCard);
+        discardPanel.add(discardRepresentation);
         
         JPanel stackPanel = new JPanel();
         GridBagConstraints gbc_stackPanel = new GridBagConstraints();
@@ -81,9 +98,19 @@ public class GameFrame extends JFrame {
         gbc_stackPanel.gridy = 3;
         contentPane.add(stackPanel, gbc_stackPanel);
         stackPanel.setLayout(new GridLayout(1, 0, 0, 0));
-        
-        JLabel stackOfCards = new JLabel("Stack of cards");
+
+        ImageIcon stackIcon = new ImageIcon("data/stack_of_cards.png");
+        JLabel stackOfCards = new JLabel(stackIcon);
         stackPanel.add(stackOfCards);
+ 
+        
+        JLabel lblNewLabel_1 = new JLabel("Kilometer cards played:");
+        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+        gbc_lblNewLabel_1.gridwidth = 2;
+        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_1.gridx = 0;
+        gbc_lblNewLabel_1.gridy = 4;
+        contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
         
         kilometerPanel = new JPanel(); 
         kilometerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
@@ -91,20 +118,20 @@ public class GameFrame extends JFrame {
         gbc_kilometerPanel.insets = new Insets(0, 0, 0, 5);
         gbc_kilometerPanel.gridwidth = 2;
         gbc_kilometerPanel.gridx = 0;
-        gbc_kilometerPanel.gridy = 4;
+        gbc_kilometerPanel.gridy = 5;
         contentPane.add(kilometerPanel, gbc_kilometerPanel);
         
-        JLabel cardLabel5 = new JLabel("Card 5");
-        addCardWithCounter(cardLabel5, 5);
+        JLabel cardLabel5 = new JLabel("| 5 Km cards: ");
+        addCardWithCounter(cardLabel5, numberof5cards);
         
-        JLabel cardLabel6 = new JLabel("Card 6");
-        addCardWithCounter(cardLabel6, 6);
+        JLabel cardLabel6 = new JLabel("| 6 Km cards: ");
+        addCardWithCounter(cardLabel6, numberof6cards);
         
-        JLabel cardLabel8 = new JLabel("Card 8");
-        addCardWithCounter(cardLabel8, 8);
+        JLabel cardLabel8 = new JLabel("| 8 Km cards: ");
+        addCardWithCounter(cardLabel8, numberof8cards);
         
-        JLabel cardLabel10 = new JLabel("Card 10");
-        addCardWithCounter(cardLabel10, 10);
+        JLabel cardLabel10 = new JLabel("| 10 Km cards: ");
+        addCardWithCounter(cardLabel10, numberof10cards);
         
         
         
