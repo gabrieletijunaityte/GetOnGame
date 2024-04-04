4e4e8d203a36d2ba21bcee3f4b64c71964d741ef
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-04 16:36:12 +0200

    YM MR GT create StatusCard class and StatusCardTest

---
+++ GetOn/src/logic/StatusCard.java
@@ -0,0 +1,12 @@
+package logic;
+
+public class StatusCard extends Card {
+
+	// Constructor
+	public StatusCard(String value) {
+		super("STATUS", value);
+
+		// Assign possible status values
+		this.pos_values = new String[] { "GET_ON", "TAIL_WIND" };
+	}
+}
---
+++ GetOn/src/test/StatusCardTest.java
@@ -0,0 +1,77 @@
+package test;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+import logic.StatusCard;
+
+public class StatusCardTest extends TestCase {
+
+
+	// Test status card creation
+	public void testCreationOfstatusCard() {
+		// Creating a status card
+		StatusCard statusCard = new StatusCard("HOSTEL");
+
+		assertNotNull(statusCard);
+	}
+
+	// Test the type retrieval method
+	public void testgetType() {
+		// Setting the target
+		String target = "STATUS";
+
+		// Creating a status card
+		StatusCard statusCard = new StatusCard("GET_ON");
+
+		// Getting the type
+		String test = statusCard.getType();
+
+		// Asserting if it is right type
+		assertEquals(target, test);
+	}
+
+	// Test the value retrieval method
+	public void testgetValue() {
+		// Setting the target
+		String target = "GET_ON";
+
+		// Creating a status  card
+		StatusCard statusCard = new StatusCard(target);
+
+		// Getting the type
+		String test = statusCard.getValue();
+
+		// Asserting if it is right type
+		assertEquals(target, test);
+	}
+
+// Test the getCardName() method
+	public void testgetCardName() {
+		// Setting the target
+		String target = "GET_ON";
+		String target1 = "STATUS_" + target;
+
+		// Creating a status card
+		StatusCard statusCard = new StatusCard(target);
+
+
+		// Testing getCardNameMethod
+		String test = statusCard.getCardName();
+
+		assertEquals(target1, test);
+	}
+	
+	// Test getPossibleValues
+	public void testGetPossibleValues() {
+		// Creating Card class
+		StatusCard statusCard = new StatusCard("GET_ON");
+		
+		// Testing getCardNameMethod
+		String test = Arrays.toString(statusCard.getPossibleValues());
+		System.out.print(Arrays.toString(statusCard.getPossibleValues()));
+		
+		assertEquals("[GET_ON, TAIL_WIND]", test);		
+	}
+
+}
