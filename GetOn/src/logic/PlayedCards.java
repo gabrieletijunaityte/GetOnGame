3b0ff12ec0dbc727c07382b75910c4769b96882e
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-05 16:55:05 +0200

    GT JW add some methods in PlayedCards class and write according tests
    Add methods of:
    - adding a card to played cards
    - getting an arrayList of played cards
    - counting how many specific card type and value combinations are present in the played cards

--- GetOn/src/logic/PlayedCards.java
+++ GetOn/src/logic/PlayedCards.java
@@ -4,47 +4,91 @@ import java.util.ArrayList;
 
 public class PlayedCards {
 	private ArrayList<Card> playedCards = new ArrayList<>();
-	
+
 	public PlayedCards(Card card) {
-		playedCards.add(card);
-		}
-	
-/*	Is still in work
- 
- 	private void isPlayable(Card card, Player currentPlayer) {
-		if (!currentPlayer.getOnBikeStatus()) {
-			if(currentPlayer.getBulliedStatus()) {
-				if (card.getType() == "REPAIR") {
-					playedCards.add(card);
-				}
-				else if(card.getType() == "BULLY") {
-// Add functionality in Player
-//					currentPlayer.playBullyCard(bulliedPlayer);
-						}
-				else if(card.getType() == "STATUS" && card.getValue() == "TAIL_WIND") {
-					playedCards.add(card);
-				}
-				else {
-					throw new IllegalArgumentException("Card cannot be played");
-				}
-			}
-			else if(card.getType() == "STATUS") {
-				playedCards.add(card)
-			}
-			else if(card.getType() == "BULLY") {
-//				currentPlayer.playBullyCard(bulliedPlayer);
-			}
-			else{
-				throw new IllegalArgumentException("Card cannot be played");
-			}
-		}
-		
-		
-		
-		String cardType = card.getType();
-		
-		
+		getPlayedCards().add(card);
 	}
-	*/
-}
 	
+	// Count how many of a specific cards have been played
+	public int countCards(Card card) {
+		// initiate count
+		int i = 0;
+		
+		// iterate over the played cards
+		for (Card playedCard : this.getPlayedCards()) {
+			
+			// check how many of the given specific cards are in the played pile
+			if (playedCard.getCardName().equals(playedCard.getCardName())) {
+				i++;
+			}
+		}
+		return i;
+	}
+
+	public void addCard(Card card1) {
+		// IMPLEMENT CHECKING
+//		
+		// Add card
+		this.getPlayedCards().add(card1);
+	}
+
+	public ArrayList<Card> getPlayedCards() {
+		return this.playedCards;
+	}
+	
+	
+
+////	Is still in work
+//
+//	// Method to play a selected card
+//	private void isPlayable(Card card, Player currentPlayer) {
+// 		// Card card - selected card
+// 		// Player currentPlayer - player who plays the card
+// 		
+// 		// Check if the player is off the bike
+//		if (!currentPlayer.getOnBikeStatus()) {
+//			
+//			// if player is off the bike, look if he was bullied
+//			if(currentPlayer.getBulliedStatus()) {
+//				
+//				// if player was bullied
+//				if (card.getType() == "REPAIR") {
+//					
+//					// if selected card is repair, play it
+//					playedCards.add(card);
+//				}
+//				else if(card.getType() == "BULLY") {
+//// Add functionality in Player
+//					// if selected card is bully card, play it
+////					currentPlayer.playBullyCard(bulliedPlayer);
+//						}
+//				else if(card.getType() == "STATUS" && card.getValue() == "TAIL_WIND") {
+//					
+//					// if selected card is "TAIL_WIND", play it
+//					playedCards.add(card);
+//				}
+//				else {
+//					// if selected card is none of the above, throw error
+//					throw new IllegalArgumentException("Card cannot be played");
+//				}
+//			}
+//			
+//			// if the player is not on a bike and is not bullied
+//			else if(card.getType() == "STATUS") {
+//				
+//				// play any status card
+//				playedCards.add(card);
+//			}
+//			else if(card.getType() == "BULLY") {
+//				
+//				// play any bully card
+////				currentPlayer.playBullyCard(bulliedPlayer);
+//			}
+//			else{
+//				
+//				// if given card is none of above throw an error
+//				throw new IllegalArgumentException("Card cannot be played");
+//			}
+//		}	
+//	}
+}
--- GetOn/src/test/PlayedCardsTest.java
+++ GetOn/src/test/PlayedCardsTest.java
@@ -5,15 +5,78 @@ import logic.Card;
 import logic.PlayedCards;
 import logic.Type;
 import logic.Value;
+import logic.KilometerCard;
 
 public class PlayedCardsTest extends TestCase {
+
 	// Test playedCard creation
 	public void testCreationOfPlayedCards() {
-		
+		// Create a fake card
 		Card card = new Card(Type.getType(0), Value.getValue(0));
-				
+		// Replace with this after merging
+//		KilometerCard card = new KilometerCard("FIVE");
+		
+		// creating played card pile by playing a first card
 		PlayedCards playedCards = new PlayedCards(card);
+		
+		// Assert if playCards instance was created
 		assertNotNull(playedCards);
 	}
 	
+	// Test addCard method
+	public void testAddCardToPlayedCards() {
+		// Create fake cards
+		Card card = new Card(Type.getType(0), Value.getValue(0));
+		Card card1 = new Card(Type.getType(1), Value.getValue(0));
+		// Replace with this after merging
+//		KilometerCard card = new KilometerCard("FIVE");
+//		KilometerCard card1 = new KilometerCard("SIX");
+		
+		// Create the played cards
+		PlayedCards playedCards = new PlayedCards(card);
+		// Add card
+		playedCards.addCard(card1);
+		
+		assertEquals(2, playedCards.getPlayedCards().size());
+	}
+	
+
+	// Test countCards method
+	public void testCountPlayedCards() {
+		// Create fake cards
+		Card card = new Card(Type.getType(0), Value.getValue(0));
+		Card card1 = new Card(Type.getType(0), Value.getValue(0));
+		
+		// Replace with this after merging
+//		KilometerCard card = new KilometerCard("FIVE");
+//		KilometerCard card1 = new KilometerCard("SIX");
+		
+		// Create the played cards
+		PlayedCards playedCards = new PlayedCards(card);
+		playedCards.addCard(card1);
+		
+		assertEquals(2, playedCards.countCards(card));		
+	}
+	
+	// Test getPlayedCards method
+	public void testGetPlayedCards() {
+		// Create fake cards
+		Card card = new Card(Type.getType(0), Value.getValue(0));
+		Card card1 = new Card(Type.getType(0), Value.getValue(0));
+		
+		// Replace with this after merging
+//		KilometerCard card = new KilometerCard("FIVE");
+//		KilometerCard card1 = new KilometerCard("SIX");
+		
+		// Create the played cards
+		PlayedCards playedCards = new PlayedCards(card);
+		playedCards.addCard(card1);
+		
+		// Assert if returned card list is as expected
+		assertEquals("[kilometer_five, kilometer_five]", playedCards.getPlayedCards().toString());		
+		// to be replaced after merge
+//		assertEquals("[KILOMETER_FIVE, KILOMETER_SIX]", playedCards.getPlayedCards().toString());		
+		
+	}
+
 }
