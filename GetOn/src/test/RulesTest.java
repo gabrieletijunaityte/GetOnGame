a6c571d1169672983beda80c4344114e3294f5fe
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-12 15:02:44 +0200

    SJ GT add isPlayble method to Rules class + test

--- GetOn/src/test/RulesTest.java
+++ GetOn/src/test/RulesTest.java
@@ -1,7 +1,14 @@
 package test;
 
+import java.util.Arrays;
+
+
 import junit.framework.TestCase;
 import logic.Rules;
+import logic.Player;
+import logic.Card;
+import logic.StatusCard;
+import logic.KilometerCard;
 
 public class RulesTest extends TestCase {
 
@@ -21,5 +28,41 @@ public class RulesTest extends TestCase {
 		assertNotNull(returned);
 		
 	}
+	
+	// Check if the card is playble caseA (kmCard with requirement for wind)
+	public void testIsPlaybleCaseA() {
+		Rules rules = new Rules();
+		
+		// Define required attributes
+		Card kmCard = new KilometerCard("EIGHT");
+		Player testPlayer = new Player("Jan", true);
+		
+		// Set Statuses for player to be able to play the card
+		testPlayer.setOnBikeStatus(true);
+		testPlayer.setHasWind(true);
+		
+		
+		// Defining test variables
+		Boolean target = true;
+		Boolean test = rules.isPlayble(kmCard, testPlayer);
+		
+		assertEquals(target, test);
+	}
+	
+	// Check if the card is playble caseB (GET_ON with requirement for noBully)
+	public void testIsPlaybleCaseB() {
+		Rules rules = new Rules();
+		
+		// Define required attributes
+		Card statusCard = new StatusCard("GET_ON");
+		Player testPlayer = new Player("Jan", true);
+		
+		// Defining test variables
+		Boolean target = true;
+		Boolean test = rules.isPlayble(statusCard, testPlayer);
+		
+		assertEquals(target, test);
+	}
+
 	
 }
