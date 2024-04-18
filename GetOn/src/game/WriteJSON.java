31031f85fe4aa70f187144be47aab03b8944de90
Gabriele Tijunaityte <gabriele.tijunaityte@wur.nl>
2024-04-18 17:06:23 +0200

    VY SJ GT import json.simple library, write json file
    + test

---
+++ GetOn/src/game/WriteJSON.java
@@ -0,0 +1,33 @@
+package game;
+
+import logic.Player;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+
+public class WriteJSON {
+	private JSONArray playerList = new JSONArray();
+
+	@SuppressWarnings("unchecked")
+	public WriteJSON(ArrayList <Player> players) {
+		for (Player player : players) {
+			this.playerList.add(player);
+		}
+
+	}
+	
+	public void writePlayers() {
+        //Write JSON file
+        try (FileWriter file = new FileWriter("data/outputs/players.json")) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(this.playerList.toJSONString()); 
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+}
